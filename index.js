3b715efd69fb96563c695551ff2440eaec8a7a32
Sasha Skryd <sashaskryd@gmail.com>
2021-06-24 18:57:41 +0300

    init server

---
+++ index.js
@@ -0,0 +1,38 @@
+const exp = require("constants");
+const express = require("express");
+const userRouter = require('./users/users.routes')
+
+const PORT = process.env.port || 5500;
+class Server {
+
+    constructor(){
+        this.server = null;
+    }
+
+    start(){
+        this.server = express();
+        this.initMiddlewares();
+        this.initRoutes();
+        this.listen();
+    }
+
+    initMiddlewares(){
+        this.server.use(express.json())
+    }
+
+    initRoutes(){
+        this.server.use('/blog', userRouter)
+    }
+
+    listen(){
+        this.server.listen(PORT,()=>{
+            console.log(`Server is listening on port ${PORT}`);
+        })
+    }
+
+    
+};
+
+const server = new Server();
+
+server.start()
