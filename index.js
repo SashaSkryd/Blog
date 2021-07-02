4e52720b9c9d7662e9d0adb9131384f0840629da
ArtemSlipchencko <pfistaskin@gmail.com>
2021-07-02 13:44:54 +0300

    fix my stupidity

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userRouter = require("./users/users.routes");
-const postRouter = require("./posts/post.router");
+// const postRouter = require("./posts/post.router");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
